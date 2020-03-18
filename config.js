f5fca9955cfe8b533858468e31dd1f2f24d6ec87
Krystian Bieniek <krystekk2013@gmail.com>
2020-03-18 21:01:44 +0100

    Add style css

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  db: "",
+  db:
+    "mongodb+srv://Krystian:htghfCREABuZgIjQ@cluster0-29pb3.mongodb.net/test?retryWrites=true&w=majority",
   keySession: ["YOURKEY"],
   maxAgeSession: 24 * 60 * 60 * 1000
 };
