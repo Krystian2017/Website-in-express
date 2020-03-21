090211ffa2ad54bb9c15638dbe56cbdff0dd25df
Krystian Bieniek <krystekk2013@gmail.com>
2020-03-21 17:28:25 +0100

    Add api

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ var indexRouter = require("./routes/index");
 var newsRouter = require("./routes/news");
 var quizRouter = require("./routes/quiz");
 var adminRouter = require("./routes/admin");
-
-const pass = "Q3T5XGRtbGmFct80";
-// mongodb+srv://Krystian:Q3T5XGRtbGmFct80@cluster0-29pb3.mongodb.net/test?retryWrites=true&w=majority
+var apiRouter = require("./routes/api");
 
 var app = express();
 
@@ -49,6 +47,7 @@ app.use("/", indexRouter);
 app.use("/news", newsRouter);
 app.use("/quiz", quizRouter);
 app.use("/admin", adminRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
